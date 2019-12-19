a3cda57ea8226a6f1a4e14a55a99ed26d2f2ac2b
Kevin Wong <kevinwong@pursuit.org>
2019-12-18 19:21:45 -0500

    complete selectBtn onchange event listener

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     let url = 'https://ghibliapi.herokuapp.com/films';
     
-    const populateSelect = async (url) => {
+    const populateSelect = async () => {
         let res = await axios.get(url);
         let films = res.data;
         films.forEach(film => {
@@ -11,7 +11,24 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     }
 
-    populateSelect(url);
+    populateSelect();
+
+    let select = document.querySelector("select");
+    select.addEventListener("change", async (e) => {
+        let res = await axios.get(url);
+        let films = res.data;
+        debugger
+        let title = document.querySelector("#title");
+        let releaseYear = document.querySelector("#releaseYear");
+        films.forEach(film => {
+            if(e.target.value === film.title){
+                title.innerText = film.title;
+                releaseYear.innerText = film.release_date;
+                description.innerText = film.description;
+            } 
+        })
+    })
+
 
     
 })
